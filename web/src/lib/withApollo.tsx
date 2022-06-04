6c055c358d424870c0366c28d3b14bb9b0fd0837
Lucas Eduardo <contato@lucaseduardo.io>
2022-06-04 00:13:38 -0300

    feat: frontend nextjs

---
+++ web/src/lib/withApollo.tsx
@@ -0,0 +1,32 @@
+import { ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import { GetServerSidePropsContext, NextPage } from "next";
+
+export type ApolloClientContext = GetServerSidePropsContext;
+
+export const getApolloClient = (
+  _?: ApolloClientContext,
+  ssrCache?: NormalizedCacheObject
+) => {
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3000/api',
+    fetch,
+  })
+  
+  const cache = new InMemoryCache().restore(ssrCache ?? {})
+  
+  return new ApolloClient({
+    link: from([httpLink]),
+    cache,
+  })
+}
+
+export const withApollo = (Component: NextPage) => {
+  return function Provider(props: any) {
+    return (
+      <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
+        <Component />
+      </ApolloProvider>
+    )
+  }
+}
+
